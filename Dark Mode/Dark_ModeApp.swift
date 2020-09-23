2a6b112428ba9d432fbd44ab7c93861d655ddae3
Mickael MAS <mickael.mas@appiwedia.com>
2020-09-23 11:28:15 +0200

    Initial Commit

---
+++ Dark Mode/Dark_ModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dark_ModeApp.swift
+//  Dark Mode
+//
+//  Created by Mickael Mas on 23/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Dark_ModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
